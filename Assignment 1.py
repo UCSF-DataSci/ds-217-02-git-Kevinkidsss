0a26b7b04e84751795aeccff0a5f896e6e2d8cd6
Kevin Sun <159496424+Kevinkidsss@users.noreply.github.com>
2024-09-22 20:43:17 +0000

    Add Assignment 1 and Assignment 2 files

---
+++ Assignment 1.py
@@ -0,0 +1,6 @@
+print ("Hello, Data Science!")
+total_sum = 0
+for num in range(1000):
+    if num % 3 == 0 or num % 5 == 0:
+        total_sum += num
+print("The sum of all multiples of 3 or 5 below 1000 is:", total_sum)
---
+++ Assignment 2.py
@@ -0,0 +1,28 @@
+#Kevin Sun
+#Data Science 217
+#09/21/2024
+
+#Clone your forked repository to your local machine
+
+#git clone https://github.com/UCSF-DataSci/ds-217-02-git-Kevinkidsss.git
+#Create and switch to a new branch
+
+#git checkout -b newbranch
+
+#Create a `readme.md` file in the new branch
+#echo "# This is the README file for the newbranch" > readme.md
+
+#Add a copy of your python file from assignment 1 to the repo
+#git add readme.md
+
+#Stage and commit the files with a commit message
+#git commit -m "Add Assignment 1.py and create readme.md"
+
+#Merge your new branch back with the `main` branch
+#git checkout main
+#git merge newbranch
+
+
+
+
+
