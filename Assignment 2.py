5907b2d91ce0ebcbfac986b515e767c4c0ea1dcc
Kevin Sun <159496424+Kevinkidsss@users.noreply.github.com>
2024-09-23 01:55:19 +0000

    Add virtual environment directory to .gitignore

--- Assignment 2.py
+++ Assignment 2.py
@@ -2,11 +2,12 @@
 #Data Science 217
 #09/21/2024
 
+#1.GIT 
+
 #Clone your forked repository to your local machine
 
 #git clone https://github.com/UCSF-DataSci/ds-217-02-git-Kevinkidsss.git
 #Create and switch to a new branch
-
 #git checkout -b newbranch
 
 #Create a `readme.md` file in the new branch
@@ -18,11 +19,34 @@
 #Stage and commit the files with a commit message
 #git commit -m "Add Assignment 1.py and create readme.md"
 
+
 #Merge your new branch back with the `main` branch
 #git checkout main
 #git merge newbranch
 
+#Push your changes to GitHub
+#git push origin newbranch
 
 
-
+#2. Markdown
+	#- Fill in your `readme.md` with the requested information from assignment 1
+    
+	#- Use formatting to separate sections using `# Header`'s
+	#- Add a link to the official Python website (https://www.python.org)
+3. Python environments & packages
+	- Create a virtual environment
+	- Activate the virtual environment
+	- Add the directory of the virtual environment to `.gitignore` (commit to repo)
+	- Use `pip install` or `python -m pip install` to install a package (okay to use the example from lecture)
+	- Take a screenshot of the installation
+	- Add the screenshot to your assignment repository
+4. Markdown
+	- Add the screenshot image to your readme.md
+	- Add a meme you like from the internet to readme.md
+	- Commit and push changes back to GitHub
+5. Check your work on GitHub, you should now have:
+	- A single branch, `main`
+	- Python file from assignment 1
+	- `.gitignore` with at least one entry for the virtual environment folder
+	- `readme.md` with assignment 1 content, plus images
 
