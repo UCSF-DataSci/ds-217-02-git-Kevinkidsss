6f8e910a8d7020b5ac98dea40d8e4c4b5c25976a
Kevin Sun <159496424+Kevinkidsss@users.noreply.github.com>
2024-09-23 05:33:07 +0000

    Add pip request screenshot to readme.md

--- Assignment 2.py
+++ Assignment 2.py
@@ -30,23 +30,44 @@
 
 #2. Markdown
 	#- Fill in your `readme.md` with the requested information from assignment 1
-    
-	#- Use formatting to separate sections using `# Header`'s
-	#- Add a link to the official Python website (https://www.python.org)
-3. Python environments & packages
-	- Create a virtual environment
-	- Activate the virtual environment
-	- Add the directory of the virtual environment to `.gitignore` (commit to repo)
-	- Use `pip install` or `python -m pip install` to install a package (okay to use the example from lecture)
-	- Take a screenshot of the installation
-	- Add the screenshot to your assignment repository
-4. Markdown
-	- Add the screenshot image to your readme.md
-	- Add a meme you like from the internet to readme.md
-	- Commit and push changes back to GitHub
-5. Check your work on GitHub, you should now have:
-	- A single branch, `main`
-	- Python file from assignment 1
-	- `.gitignore` with at least one entry for the virtual environment folder
-	- `readme.md` with assignment 1 content, plus images
 
+	#- Use formatting to separate sections using `# Header`'s
+
+	#- Add a link to the official Python website (https://www.python.org)
+
+#3. Python environments & packages
+	#- Create a virtual environment
+    #python -m venv myproject_env
+
+	#- Activate the virtual environment
+    #source myproject_env/bin/activate
+
+	#- Add the directory of the virtual environment to `.gitignore` (commit to repo)
+    #myproject_env/
+    #git add .gitignore
+    #git commit -m "Add virtual environment directory to .gitignore"
+
+	#- Use `pip install` or `python -m pip install` to install a package (okay to use the example from lecture)
+    #pip install requests
+
+	#- Take a screenshot of the installation
+
+	#- Add the screenshot to your assignment repository
+
+#4. Markdown
+	#- Add the screenshot image to your readme.md
+    #![pip installation](C:\Users\kevin\OneDrive\Desktop\UCSF Fall 2024\Datasci 217\Intro to Python\hw\pip request.png "pip installation")
+    #git add images/pip_request.png readme.md
+    #git commit -m "Add pip request screenshot to readme.md"
+    #git push origin main
+
+	#- Add a meme you like from the internet to readme.md
+    #![Meme](https://i0.wp.com/copyhackers.com/wp-content/uploads/2020/06/img_20200130_0253004222178807860404007.jpg?w=890&ssl=1)
+
+	#- Commit and push changes back to GitHub
+
+#5. Check your work on GitHub, you should now have:
+	#- A single branch, `main`
+	#- Python file from assignment 1
+	#- `.gitignore` with at least one entry for the virtual environment folder
+	#- `readme.md` with assignment 1 content, plus images
